6460f776623bd7261a219add0b20559c8f621647
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-02-12 20:55:58 +0100

    option to not encode + cleanup

--- save_file_manager.py
+++ save_file_manager.py
@@ -1,21 +1,24 @@
+"""
+This module allows a basic (save) file creation, loading and deletion interface, with "secure" encoding support
+"""
 import numpy as np
 import base64
 import os
 
 
 def imput(ask="Num: "):
-    """""""""""""""
+    """
     Input but only accepts whole numbers.
-    """""""""
+    """
     while True:
         try: return int(input(ask))
         except ValueError: print("Not number!")
 
 
 def encode_save(save_file=[], save_num=1, save_name="save*", save_ext="sav"):
-    """""""""""""""
+    """
     Creates a file that has been encoded, from a list.
-    """""""""
+    """
     f = open(f'{save_name.replace("*", str(save_num))}.{save_ext}', "wb")
     r = np.random.RandomState(int(save_num * np.pi * 3853))
     encode_64 = r.randint(3, 10)
@@ -30,16 +33,16 @@ def encode_save(save_file=[], save_num=1, save_name="save*", save_ext="sav"):
             line_bytes_enc.append(byte + r.randint(1, 134))
         line_bytes_enc.append(10)
         f.write(bytes(line_bytes_enc))
-    f.close
+    f.close()
 
 
 def decode_save(save_num=1, save_name="save*", save_ext="sav"):
-    """""""""""""""
+    """
     Returns a list of strings, decoded fron the encoded file.
-    """""""""
+    """
     f = open(f'{save_name.replace("*", str(save_num))}.{save_ext}', "rb")
     lines = f.readlines()
-    f.close
+    f.close()
     lis = []
     r = np.random.RandomState(int(save_num * np.pi * 3853))
     encode_64 = r.randint(3, 10)
@@ -57,10 +60,10 @@ def decode_save(save_num=1, save_name="save*", save_ext="sav"):
     return lis
 
 
-def file_reader(max_saves=5, write_out=False, save_name="save*", save_ext="sav"):
-    """""""""""""""
-    Returns encripted data from all save files with the save file number.
-    """""""""
+def file_reader(max_saves=5, write_out=False, save_name="save*", save_ext="sav", is_file_encoded=True):
+    """
+    Returns (encripted) data from all save files with the save file number.
+    """
     file_data = []
     file_num = 0
     if write_out:
@@ -73,7 +76,15 @@ def file_reader(max_saves=5, write_out=False, save_name="save*", save_ext="sav")
         except FileNotFoundError:
             pass
         else:
-            data = decode_save(file_num, save_name, save_ext)
+            if is_file_encoded:
+                data = decode_save(file_num, save_name, save_ext)
+            else:
+                data = []
+                f = open(f'{save_name.replace("*", str(file_num))}.{save_ext}', "r")
+                lines = f.readlines()
+                f.close()
+                for line in lines:
+                    data.append(line.replace("\n", ""))
             file_data.append([file_num, data])
             if write_out:
                 print(f"Save File {file_num}:")
@@ -83,13 +94,13 @@ def file_reader(max_saves=5, write_out=False, save_name="save*", save_ext="sav")
 
 
 def manage_saves(file_data=[], max_saves=5, save_name="save*", save_ext="sav"):
-    """""""""""""""
+    """
     Allows the user to pick between creating a new save, loading an old save and deleteing a save.\n
     Returns the option the user selected:\n
     \t[0, x] = load, into x slot\n
     \t[1, x] = new file, into x slot\n
     \t[-1] = deleted file
-    """""""""
+    """
     manage_exit = False
     while not manage_exit:
         if len(file_data):
@@ -132,25 +143,45 @@ def manage_saves(file_data=[], max_saves=5, save_name="save*", save_ext="sav"):
     return [-1]
 
 
-def default_run(max_saves=5, save_name="save*", save_ext="sav", write_out=True):
+def default_run(max_saves=5, save_name="save*", save_ext="sav", write_out=True, is_file_encoded=True):
     save = ["bro dude 42069", "áéűől4"]
     save_new = ["loading lol 69", "űűűűűűűűűűűűűűűűűűűűűűááááááááááááűáűáűááááááááá"]
-    encode_save(save, 1, save_name, save_ext)
-    encode_save(save, 2, save_name, save_ext)
-    encode_save(save, 4, save_name, save_ext)
+    if is_file_encoded:
+        encode_save(save, 1, save_name, save_ext)
+        encode_save(save, 2, save_name, save_ext)
+        encode_save(save, 4, save_name, save_ext)
+    else:
+        f = open(f'{save_name.replace("*", str(1))}.{save_ext}', "w")
+        for line in save:
+            f.write(line + "\n")
+        f.close()
+        f = open(f'{save_name.replace("*", str(2))}.{save_ext}', "w")
+        for line in save:
+            f.write(line + "\n")
+        f.close()
+        f = open(f'{save_name.replace("*", str(4))}.{save_ext}', "w")
+        for line in save:
+            f.write(line + "\n")
+        f.close()
     options_status = True
     while options_status:
-        datas = file_reader(max_saves, write_out, save_name, save_ext)
+        datas = file_reader(max_saves, write_out, save_name, save_ext, is_file_encoded)
         status = manage_saves(datas, max_saves, save_name, save_ext)
         if status[0] == 1 or status[0] == 0:
             options_status = False
     if status[0] == 1:
         fff = input(f"NEW GAME!!! in {status[1]}")
-        encode_save(save_new, status[1], save_name, save_ext)
+        if is_file_encoded:
+            encode_save(save_new, status[1], save_name, save_ext)
+        else:
+            f = open(f'{save_name.replace("*", str(status[1]))}.{save_ext}', "w")
+            for line in save_new:
+                f.write(line + "\n")
+            f.close()
     elif status[0] == 0:
         fff = input(f"LOADING SAVE {status[1]}!!!")
 
 
-default_run(5, "save*", "sav", True)
+# default_run(5, "save*", "sav", True, False)
 
-#test_save = ["test testtest 42096 éáőúűá", "line", "linelinesabnjvaqxcyvíbíxmywjefgsetiuruoúpőáűégfgk,v.mn.--m,1372864594"]
+# test_save = ["test testtest 42096 éáőúűá", "line", "linelinesabnjvaqxcyvíbíxmywjefgsetiuruoúpőáűégfgk,v.mn.--m,1372864594"]
