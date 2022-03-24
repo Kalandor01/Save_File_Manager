c0b21130b414025fc335f9cb04b54ece5078523a
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-24 15:54:14 +0100

    turned list  into an object + getkey function

--- save_file_manager/__init__.py
+++ save_file_manager/__init__.py
@@ -3,7 +3,7 @@ This module allows a basic (save) file creation, loading and deletion interface,
 It also has a function for a list choice UI.\n
 Use 'save_name = os.path.dirname(os.path.abspath(__file__)) + "/save*"' as the save name to save files in the current directory instead of the default path.
 """
-__version__ = '1.6.2'
+__version__ = '1.6.3'
 
 from numpy import random as npr
 
@@ -153,6 +153,38 @@ def manage_saves(file_data=[], max_saves=5, save_name="save*", save_ext="sav"):
     return [-1, option]
 
 
+def get_key():
+    """
+    Function for detecting a keypress (mainly arrow keys)\n
+    Returns a number depending on the key type (0-5), and -1 if msvcrt/getch was not found.\n
+    Returned keys for numbers (0-5): [ESC][UP][DOWN][LEFT][RIGHT][ENTER]
+    """
+    try:
+        from msvcrt import getch
+    except ModuleNotFoundError:
+        input("\n\nmsvcrt MODULE NOT FOUND!\nTHIS MODULE IS WINDOWS ONLY!\n\n")
+        return -1
+    
+    arrow = False
+    while True:
+        key = getch()
+        # print(key)
+        if key == b"\x1b":
+            return 0
+        if arrow and key == b"H":
+            return 1
+        elif arrow and key == b"P":
+            return 2
+        elif arrow and key == b"K":
+            return 3
+        elif arrow and key == b"M":
+            return 4
+        elif key == b"\r":
+            return 5
+        arrow = False
+        if key == b"\xe0" or key == b"\x00":
+            arrow = True
+
 class Slider:
     """
     Object for the slider_ui method\n
@@ -182,18 +214,14 @@ def slider_ui(sliders=["Template", Slider(pre_text="template", display_value=Tru
     Accepts a list of mainly Slider objects.\n
     if an element in the list is not a slider object, it will be printed, (or if it's None, the line will be blank) and cannot be selected.
     """
-    try:
-        from msvcrt import getch
-    except ModuleNotFoundError:
-        input("\n\nmsvcrt MODULE NOT FOUND!\nTHIS MODULE IS WINDOWS ONLY!\n\n")
     
     selected = 0
     while type(sliders[selected]) != Slider:
         selected += 1
         if selected > len(sliders) - 1:
             selected = 0
-    action = 0    
-    while action != 5:
+    key = 0    
+    while key != 5:
         # render
         # clear screen
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -250,29 +278,13 @@ def slider_ui(sliders=["Template", Slider(pre_text="template", display_value=Tru
                 except AttributeError:
                     print(sliders[x])
         # slider select
-        arrow = False
-        action = 0
-        while action == 0:
-            key = getch()
-            # b"\x1b" = esc
-            # print(key)
-            if arrow and key == b"P":
-                action = 1
-            elif arrow and key == b"H":
-                action = 2
-            elif arrow and key == b"M":
-                action = -1
-            elif arrow and key == b"K":
-                action = -2
-            elif key == b"\r":
-                action = 5
-            arrow = False
-            if key == b"\xe0" or key == b"\x00":
-                arrow = True
+        key = 0
+        while key == 0:
+            key = get_key()
         # move selection
-        if 0 < action < 5:
+        if 1 <= key <= 2:
             while True:
-                if action == 1:
+                if key == 2:
                     selected += 1
                     if selected > len(sliders) - 1:
                         selected = 0
@@ -283,9 +295,9 @@ def slider_ui(sliders=["Template", Slider(pre_text="template", display_value=Tru
                 if type(sliders[selected]) == Slider:
                     break
         # move slider
-        elif 0 > action:
+        elif 3 <= key <= 4:
             while True:
-                if action == -1:
+                if key == 4:
                     if sliders[selected].value + sliders[selected].section.step <= sliders[selected].section.stop:
                         sliders[selected].value += sliders[selected].section.step
                 else:
@@ -295,74 +307,123 @@ def slider_ui(sliders=["Template", Slider(pre_text="template", display_value=Tru
                     break
 
 
-def list_ui(answers=["Yes", "No"], question=None, multiline=False, selected_icon=">", not_selected_icon=" ", selected_icon_right="", not_selected_icon_right=""):
+class UI_list:
+    def __init__(self, answers=["No", "Yes"], question=None, multiline=False, selected_icon=">", not_selected_icon=" ", selected_icon_right="", not_selected_icon_right=""):
+        self.answers = list(answers)
+        self.question = str(question)
+        self.multiline = bool(multiline)
+        self.s_icon = str(selected_icon)
+        self.icon = str(not_selected_icon)
+        self.s_icon_r = str(selected_icon_right)
+        self.icon_r = str(not_selected_icon_right)
+
+
+    def display(self):
+        """
+        Prints the question and then the list of answers that the user can cycle between with the arrow keys and select with enter.\n
+        Gives back a number from 0-n acording to the size of the list that was passed in.\n
+        if the answer is None the line will be blank and cannot be selected. \n
+        Multiline makes the "cursor" draw at every line if the text is multiline.
+        """
+        
+        selected = 0
+        while self.answers[selected] == None:
+            selected += 1
+            if selected > len(self.answers) - 1:
+                selected = 0
+        key = 0
+        while key != 5:
+            # render
+            # clear screen
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            if self.question != None:
+                print(self.question + "\n")
+            for x in range(len(self.answers)):
+                if self.answers[x] != None:
+                    if selected != x:
+                        if self.multiline:
+                            print(self.icon + self.answers[x].replace("\n", f"{self.icon_r}\n{self.icon}") + self.icon_r)
+                        else:
+                            print(self.icon + self.answers[x] + self.icon_r)
+                    else:
+                        if self.multiline:
+                            print(self.s_icon + self.answers[x].replace("\n", f"{self.s_icon_r}\n{self.s_icon}") + self.s_icon_r)
+                        else:
+                            print(self.s_icon + self.answers[x] + self.s_icon_r)
+                else:
+                    print()
+            # answer select
+            key = get_key()
+            while key != 1 and key != 2 and key != 5:
+                key = get_key()
+            # move selection
+            if key != 5:
+                while True:
+                    if key == 2:
+                        selected += 1
+                        if selected > len(self.answers) - 1:
+                            selected = 0
+                    else:
+                        selected -= 1
+                        if selected < 0:
+                            selected = len(self.answers) - 1
+                    if self.answers[selected] != None:
+                        break
+        return selected
+
+def _menu_ui(layers=[]):
     """
-    Prints the question and then the list of answers that the user can cycle between with the arrow keys and select with enter.\n
-    Gives back a number from 0-n acording to the size of the list that was passed in.\n
-    if the answer is None the line will be blank and cannot be selected. \n
-    Multiline makes the "cursor" draw at every line if the text is multiline.
+    PROTOTYPE AND WRONG!!!
     """
-    try:
-        from msvcrt import getch
-    except ModuleNotFoundError:
-        input("\n\nmsvcrt MODULE NOT FOUND!\nTHIS MODULE IS WINDOWS ONLY!\n\n")
+    layers = []
+    layers.append("LIST OBJECT HERE?! + FUNCTION?")
     
-    selected = 0
-    while answers[selected] == None:
-        selected += 1
-        if selected > len(answers) - 1:
-            selected = 0
-    action = -1    
-    while action != 2:
-        # render
-        # clear screen
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        if question != None:
-            print(question + "\n")
-        for x in range(len(answers)):
-            if answers[x] != None:
-                if selected != x:
-                    if multiline:
-                        print(not_selected_icon + answers[x].replace("\n", f"{not_selected_icon_right}\n{not_selected_icon}") + not_selected_icon_right)
-                    else:
-                        print(not_selected_icon + answers[x] + not_selected_icon_right)
-                else:
-                    if multiline:
-                        print(selected_icon + answers[x].replace("\n", f"{selected_icon_right}\n{selected_icon}") + selected_icon_right)
-                    else:
-                        print(selected_icon + answers[x] + selected_icon_right)
+    while True:
+        if in_main_menu:
+            in_main_menu = False
+            option = UI_list(["New save", "Load/Delete save"], " Main menu").display()
+        else:
+            option = 1
+        # new file
+        if option == 0:
+            new_slot = 1
+            for data in file_data:
+                if data[0] == new_slot:
+                    new_slot += 1
+            if new_slot <= max_saves:
+                return [1, new_slot]
             else:
-                print()
-        # answer select
-        arrow = False
-        action = -1
-        while action == -1:
-            key = getch()
-            # b"\x1b" = esc
-            # print(key)
-            if arrow and key == b"P":
-                action = 0
-            elif arrow and key == b"H":
-                action = 1
-            elif key == b"\r":
-                action = 2
-            arrow = False
-            if key == b"\xe0" or key == b"\x00":
-                arrow = True
-        # move selection
-        if action != 2:
-            while True:
-                if action == 0:
-                    selected += 1
-                    if selected > len(answers) - 1:
-                        selected = 0
-                else:
-                    selected -= 1
-                    if selected < 0:
-                        selected = len(answers) - 1
-                if answers[selected] != None:
-                    break
-    return selected
+                input(f"No empty save files! Delete a file to continue!")
+        # load/delete
+        else:
+            # get data from file_data
+            list_data = []
+            for data in file_data:
+                list_data.append(f"{data[0]}. {data[1]}")
+            list_data.append(None)
+            list_data.append("Delete file")
+            list_data.append("Back")
+            option = UI_list(list_data, " Level select").display()
+            # load
+            if option < len(file_data):
+                return [0, file_data[option][0]]
+            # delete
+            elif option == len(file_data) + 1:
+                list_data.pop(len(list_data) - 2)
+                delete_mode = True
+                while delete_mode and len(file_data) > 0:
+                    option = UI_list(list_data, " Delete mode!", "X ", "  ").display()
+                    if option != len(list_data) - 1:
+                        sure = UI_list(["No", "Yes"], f" Are you sure you want to remove Save file {file_data[option][0]}?").display()
+                        if sure == 1:
+                            remove(f'{save_name.replace("*", str(file_data[option][0]))}.{save_ext}')
+                            list_data.pop(option)
+                            file_data.pop(option)
+                    else:
+                        delete_mode = False
+            # back
+            else:
+                in_main_menu = True
 
 
 def manage_saves_ui(file_data=[], max_saves=5, save_name="save*", save_ext="sav"):
@@ -379,7 +440,7 @@ def manage_saves_ui(file_data=[], max_saves=5, save_name="save*", save_ext="sav"
         if len(file_data):
             if in_main_menu:
                 in_main_menu = False
-                option = list_ui(["New save", "Load/Delete save"], " Main menu")
+                option = UI_list(["New save", "Load/Delete save"], " Main menu").display()
             else:
                 option = 1
             # new file
@@ -401,7 +462,7 @@ def manage_saves_ui(file_data=[], max_saves=5, save_name="save*", save_ext="sav"
                 list_data.append(None)
                 list_data.append("Delete file")
                 list_data.append("Back")
-                option = list_ui(list_data, " Level select")
+                option = UI_list(list_data, " Level select").display()
                 # load
                 if option < len(file_data):
                     return [0, file_data[option][0]]
@@ -410,9 +471,9 @@ def manage_saves_ui(file_data=[], max_saves=5, save_name="save*", save_ext="sav"
                     list_data.pop(len(list_data) - 2)
                     delete_mode = True
                     while delete_mode and len(file_data) > 0:
-                        option = list_ui(list_data, " Delete mode!", "X ", "  ")
+                        option = UI_list(list_data, " Delete mode!", "X ", "  ").display()
                         if option != len(list_data) - 1:
-                            sure = list_ui(["No", "Yes"], f" Are you sure you want to remove Save file {file_data[option][0]}?")
+                            sure = UI_list(["No", "Yes"], f" Are you sure you want to remove Save file {file_data[option][0]}?").display()
                             if sure == 1:
                                 remove(f'{save_name.replace("*", str(file_data[option][0]))}.{save_ext}')
                                 list_data.pop(option)
@@ -478,7 +539,7 @@ def _test_run(max_saves=5, save_name="save*", save_ext="sav", write_out=True, is
 # _test_run(5, "save*", "sav", True, True)
 # test_save = ["test testtest 42096 éáőúűá", "line", "linelinesabnjvaqxcyvíbíxmywjefgsetiuruoúpőáűégfgk,v.mn.--m,1372864594"]
 # test_save = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*-+,.-;>*?:_<>#&@{}<¤ß$ŁłÍ÷×¸¨"]
-# print(list_ui(["\n1", "\n2", "\n3", None, None, None, "Back", None, None, "\n\n\nlol\n"], "Are you old?", True, "-->", "  #", "<--", "#  "))
+# print(UI_list(["\n1", "\n2", "\n3", None, None, None, "Back", None, None, "\n\n\nlol\n"], "Are you old?", True, "-->", "  #", "<--", "#  ").display())
 # sliders = []
 # sliders.append(Slider(13, 5, "\ntest 1\n|", "#", "-", "|\n", True, "$\n", True))
 # sliders.append(None)
